99a454113db05ebf71a9d7541988d914f7caaaad
Ozarks <45905689+Marty-Byrde@users.noreply.github.com>
2023-05-22 22:05:03 +0200

    feat: added basic express setup

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
+import express, { Express, Request, Response } from "express"
+import bodyParser from "body-parser"
+import { join } from "path"
+import * as dotenv from 'dotenv'
+dotenv.config({ path: join(__dirname, ".env") })
 
 
-export async function myFunction(){
+const app: Express = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '5gb' }));
 
-}
+app.listen(process.env.PORT, () => console.log(`Server has been started on port ${process.env.PORT}`))
+
+app.get("/", (req: Request, res: Response) => {
+    console.log("Root page loaded!")
+    res.sendStatus(202)
+})
+
+app.post("/", (req: Request, res: Response) => {
+    const body = req.body
+    res.send(body)
+})
